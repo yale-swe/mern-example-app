f26c80d7b61857df0abe9821574607ad376bdab0
wu-json <jason.wu2153@gmail.com>
2022-02-13 16:37:06 -0500

    feat: add startApp

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,17 @@
+import mongoose from "mongoose";
 import app from "./app";
 
-const port = process.env.PORT || 4000;
+const startApp = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/mydatabase");
+    const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Server is listening at: localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(`Server is ready at: localhost:${port} 🐶`);
+    });
+  } catch (e) {
+    console.error(`Failed to start app with error 💣: ${e}`);
+  }
+};
+
+startApp();
