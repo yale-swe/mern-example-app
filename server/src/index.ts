1c60c8a3a8729afc1988a4c8a66128d49c0f496e
wu-json <jason.wu2153@gmail.com>
2022-02-13 17:22:37 -0500

    feat: adjust uri

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,16 +2,15 @@ import mongoose from "mongoose";
 import app from "./app";
 import routes from "./routes";
 
+const port = process.env.PORT || 4000;
+const mongoUri =
+  process.env.MONGO_URI ||
+  "mongodb://user:pass@localhost:27017/mydatabase?authSource=admin";
+
 const startApp = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://user:pass@localhost:27017/mydatabase?authSource=admin"
-    );
-    const port = process.env.PORT || 4000;
-
-    app.use(routes);
-
-    app.listen(port, () => {
+    await mongoose.connect(mongoUri);
+    app.use(routes).listen(port, () => {
       console.log(`Server is ready at: localhost:${port} 🐶`);
     });
   } catch (e) {
